65b183ec2057379c04ad74b8181381d12e2089c0
devcloudarmel <devcloudarmel@gmail.com>
2024-11-29 14:51:57 +0300

    Test

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,13 @@ pipeline {
     stages {
         stage('Clone repository') {
             steps {
-                
+                git branch: 'main', url: 'git@github.com:armeldevops/pipelinetest.git'
+            }
+        }
+        stage('Execute Script') {
+            steps {
+                sh 'chmod +x message.sh'
+                sh './message.sh'
             }
         }
     }
